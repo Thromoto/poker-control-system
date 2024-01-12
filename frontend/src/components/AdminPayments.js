e9431ff21acfaa80d6f0acda40169ef524c3f832
Thromoto <thi.omoto@gmail.com>
2024-01-12 20:28:26 -0300

    arrumando o css do adminpayments

--- frontend/src/components/AdminPayments.js
+++ frontend/src/components/AdminPayments.js
@@ -5,119 +5,127 @@ import { format } from "date-fns-tz";
 import "./AdminPayments.css";
 
 const AdminPayments = () => {
-    const [paymentRequests, setPaymentRequests] = useState([]);
+  const [paymentRequests, setPaymentRequests] = useState([]);
 
-    useEffect(() => {
-      // Carregar solicitações de pagamento quando o componente for montado
-      const fetchPaymentRequests = async () => {
-        try {
-          const token = localStorage.getItem("token");
-          const response = await axios.get(
-            "http://localhost:3001/api/admin/payment-requests",
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          );
-          setPaymentRequests(response.data);
-        } catch (error) {
-          console.error("Error fetching payment requests", error);
-        }
-      };
-  
-      fetchPaymentRequests();
-    }, []);
-  
-    const handleUpdateStatus = async (requestId, newStatus) => {
+  useEffect(() => {
+    // Carregar solicitações de pagamento quando o componente for montado
+    const fetchPaymentRequests = async () => {
       try {
-        // Obter o token de autorização do localStorage
         const token = localStorage.getItem("token");
-    
-        // Configurar o cabeçalho de autorização
-        const config = {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        };
-    
-        // Enviar a solicitação com o cabeçalho de autorização
-        await axios.put(
-          `http://localhost:3001/api/admin/payment-requests/${requestId}`,
-          { status: newStatus },
-          config
+        const response = await axios.get(
+          "http://localhost:3001/api/admin/payment-requests",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
         );
-    
-        // Atualizar localmente o status da solicitação de pagamento
-        setPaymentRequests((prevRequests) =>
-          prevRequests.map((request) =>
-            request._id === requestId ? { ...request, status: newStatus } : request
-          )
-        );
+        setPaymentRequests(response.data);
       } catch (error) {
-        console.error("Error updating payment request status", error);
+        console.error("Error fetching payment requests", error);
       }
     };
-  
-    const formatDateTime = (dateTimeString) => {
-      const formattedDate = format(
-        new Date(dateTimeString),
-        "dd/MM/yyyy HH:mm:ss", // Use XXX para incluir o offset de fuso horário
-        { timeZone: "America/Sao_Paulo" } // Defina o fuso horário desejado
+
+    fetchPaymentRequests();
+  }, []);
+
+  const handleUpdateStatus = async (requestId, newStatus) => {
+    try {
+      // Obter o token de autorização do localStorage
+      const token = localStorage.getItem("token");
+
+      // Configurar o cabeçalho de autorização
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      // Enviar a solicitação com o cabeçalho de autorização
+      await axios.put(
+        `http://localhost:3001/api/admin/payment-requests/${requestId}`,
+        { status: newStatus },
+        config
       );
-      return formattedDate;
-    };
-  
-    return (
-      <div className="body-admin-payments">
-        <h1>Payment Requests</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Player</th>
-              <th>Value</th>
-              <th>Day</th>
-              <th>Status</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {paymentRequests.map((request) => (
-              <tr key={request._id}>
-                {request.createdBy && request.createdBy.name ? (
-                  <td>{request.createdBy.name}</td>
-                ) : (
-                  <td>User Name Not Available</td>
-                )}
-                <td>{request.value}</td>
-                <td>{formatDateTime(request.day)}</td>
-                <td>{request.status}</td>
-                <td>
-                  {request.status === "PENDING" && (
-                    <>
-                      <button
-                        onClick={() =>
-                          handleUpdateStatus(request._id, "APPROVED")
-                        }
-                      >
-                        Approve
-                      </button>
-                      <button
-                        onClick={() =>
-                          handleUpdateStatus(request._id, "REJECTED")
-                        }
-                      >
-                        Reject
-                      </button>
-                    </>
-                  )}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
+
+      // Atualizar localmente o status da solicitação de pagamento
+      setPaymentRequests((prevRequests) =>
+        prevRequests.map((request) =>
+          request._id === requestId
+            ? { ...request, status: newStatus }
+            : request
+        )
+      );
+    } catch (error) {
+      console.error("Error updating payment request status", error);
+    }
   };
+
+  const formatDateTime = (dateTimeString) => {
+    const formattedDate = format(
+      new Date(dateTimeString),
+      "dd/MM/yyyy HH:mm:ss", // Use XXX para incluir o offset de fuso horário
+      { timeZone: "America/Sao_Paulo" } // Defina o fuso horário desejado
+    );
+    return formattedDate;
+  };
+
+  return (
+    <div className="body-admin-payments">
+      <h1>Payment Requests</h1>
+      <div className="container-admin-payments">
+        <div className="report-table-payments">
+          <table className="table-control">
+            <thead>
+              <tr>
+                <th>Player</th>
+                <th>Value</th>
+                <th>Day</th>
+                <th>Status</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {paymentRequests.map((request) => (
+                <tr key={request._id}>
+                  {request.createdBy && request.createdBy.name ? (
+                    <td>{request.createdBy.name}</td>
+                  ) : (
+                    <td>User Name Not Available</td>
+                  )}
+                  <td>$ {request.value}</td>
+                  <td>{formatDateTime(request.day)}</td>
+                  <td>{request.status}</td>
+                  <td>
+                    {request.status === "PENDING" && (
+                      <>
+                        <button
+                          className="button-admin-payments"
+                          onClick={() =>
+                            handleUpdateStatus(request._id, "APPROVED")
+                          }
+                        >
+                          Approve
+                        </button>
+                        <button
+                          className="button-admin-payments1"
+                          onClick={() =>
+                            handleUpdateStatus(request._id, "REJECTED")
+                          }
+                        >
+                          Reject
+                        </button>
+                      </>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AdminPayments;
