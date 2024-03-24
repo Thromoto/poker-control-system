db67e0e976b7d472f01f2ec66bcb3719e03ccb96
Thromoto <thi.omoto@gmail.com>
2024-03-24 18:30:26 -0300

    fixing

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import db from "./src/database/db.js";
 import routes from "./src/routes/routes.js";
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
